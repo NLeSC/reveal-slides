3423b4f7c951ec9e576b8237267e04d84365e9db
Peter Kalverla <peter.kalverla@gmx.com>
2022-10-19 14:11:44 +0200

    Add plugins and options from template

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
 import 'reveal.js/dist/reveal.css'
-// see available themes in the
-// node_modules/reveal.js/dist/theme
-//  beige, black, blood, league, moon, night, serif, simple, ...
 import './reveal-nlesc.scss'
 import Reveal from 'reveal.js'
+import Highlight from 'reveal.js/plugin/highlight/highlight.esm.js';
+import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+import Math from 'reveal.js/plugin/math/math.esm.js';
+import Notes from 'reveal.js/plugin/notes/notes.esm.js';
+import Search from 'reveal.js/plugin/search/search.esm.js';
+import Zoom from 'reveal.js/plugin/zoom/zoom.esm.js';
 
 const deck = new Reveal()
-deck.initialize({ hash: true, slideNumber: true })
+deck.initialize({
+  hash: true,
+  controls: true,
+  progress: true,
+  center: true,
+  hash: true,
+  transition: 'slide', // none/fade/slide/convex/concave/zoom
+  // menu: { // Menu works best with font-awesome installed: sudo apt-get install fonts-font-awesome
+  //   themes: false,
+  //   transitions: false,
+  //   markers: true,
+  //   hideMissingTitles: true,
+  // },
+  // dependencies: [
+  //   {src: 'reveal.js/4.3.1/reveal.js-plugins/reveal.js-d3/reveald3.js'}
+  // ],
+  plugins: [ Highlight, Markdown, Math.KaTeX, Notes, Search, Zoom ],  // Menu from separate reveal.js plugins
+})
