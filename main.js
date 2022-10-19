8c5f9c2d1322f1a14dadb562b5b8b46ae75856cb
Peter Kalverla <peter.kalverla@gmx.com>
2022-10-19 15:06:34 +0200

    Enable reveald3.js as custom plugin

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ import Math from 'reveal.js/plugin/math/math.esm.js';
 import Notes from 'reveal.js/plugin/notes/notes.esm.js';
 import Search from 'reveal.js/plugin/search/search.esm.js';
 import Zoom from 'reveal.js/plugin/zoom/zoom.esm.js';
+import { Reveald3 } from './plugin/reveald3.js';
 
 const deck = new Reveal()
 deck.initialize({
@@ -22,8 +23,5 @@ deck.initialize({
   //   markers: true,
   //   hideMissingTitles: true,
   // },
-  // dependencies: [
-  //   {src: 'reveal.js/4.3.1/reveal.js-plugins/reveal.js-d3/reveald3.js'}
-  // ],
-  plugins: [ Highlight, Markdown, Math.KaTeX, Notes, Search, Zoom ],  // Menu from separate reveal.js plugins
+  plugins: [ Highlight, Markdown, Math.KaTeX, Notes, Search, Zoom, Reveald3 ],  // Menu from separate reveal.js plugins
 })
