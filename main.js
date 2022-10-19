5cb0e268740cc156be103b064ba63bac21bfd358
Peter Kalverla <peter.kalverla@gmx.com>
2022-10-19 12:45:41 +0200

    Basic eScience theming

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import 'reveal.js/dist/reveal.css'
 // see available themes in the
 // node_modules/reveal.js/dist/theme
 //  beige, black, blood, league, moon, night, serif, simple, ...
-import './custom-style.scss'
+import './reveal-nlesc.scss'
 import Reveal from 'reveal.js'
 
 const deck = new Reveal()
