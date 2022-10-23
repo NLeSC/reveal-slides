f7b6b996331fe030e21d3f5249552137e412966d
Peter Kalverla <peter.kalverla@gmx.com>
2022-10-23 14:43:00 +0200

    More css, less js

--- plugin/decorations.js
+++ plugin/decorations.js
@@ -1,205 +1,44 @@
 // decorations.js
 //
-// This plugin will insert the following decorations:
-// - purple-overlay
-// - blue-overlay
-// - white-overlay
-// - black-overlay
-//
-// - logo-color
-// - logo-part-white
-// - logo-white
-// - right-e
-//
-// - blue-strip
-// - blue-pane-right
-// - blue-pane-left
-// - yellow-flag
-// - yellow-strip
-// - yellow-half-strip
-// - purple-blob
-// - purple-strip-bottom
-// - purple-half-circle-top
-// - purple-half-circle-bottom
-//
-// - touch-pane
-//
-// Decorations can be triggered by adding the data-state attribute to a section, e.g.
+// This plugin inserts decorations into the reveal-viewport element on load.
+// Visibility of decorations is controlled through data-state attribute of a
+// section element, e.g.
 // <section data-state="yellow-strip">
+// will toggle the yellow-strip element.
+//
+// Element styles are defined in reveal-nlesc.scss
 
 export default {
     id: 'decorations',
     init: ( deck ) => {
       initDecorations(deck);
-
-      // Update decorations on slide change event
-      deck.on( 'slidechanged', event => {
-        updateDecorations(deck);
-      });
     }
   }
-
-function updateDecorations( deck ) {
-  const currentSlide = deck.getCurrentSlide();
-  var dataState = currentSlide.getAttribute('data-state') || [];
-  const decorations = [
-    'purple-overlay',
-    'blue-overlay',
-    'white-overlay',
-    'black-overlay',
-    'logo-color',
-    'logo-part-white',
-    'logo-white',
-    'right-e-top',
-    'right-e-bottom',
-    'blue-strip',
-    'blue-pane-right',
-    'blue-pane-left',
-    'touch-pane',
-    'yellow-flag',
-    'yellow-strip',
-    'yellow-half-strip',
-    'purple-blob',
-    'purple-strip-bottom',
-    'purple-half-circle-top',
-    'purple-half-circle-bottom',
-  ];
-
-  // First process compound states
-  const compoundStates = {
-    'standard': ' logo yellow-flag white-overlay',
-    'two-pane': ' logo yellow-flag white-overlay blue-pane-right',
-    'about': ' logo white-overlay blue-pane-left purple-half-circle-top purple-strip-bottom yellow-half-strip',
-    'touch': ' logo blue-overlay touch-pane purple-blob right-e-bottom',
-  }
-
-  for (var key in compoundStates) {
-    if (dataState.includes(key)){
-      dataState += compoundStates[key]
-    }
-  }
-
-  // Decide on the logo
-  if (dataState.includes("logo")) {
-    if (dataState.includes("purple-overlay")) {
-      dataState += " logo-part-white";
-    } else if (dataState.includes("white-overlay")) {
-      dataState += " logo-color";
-    } else if (dataState.includes("black-overlay")) {
-      dataState += " logo-part-white";
-    } else if (dataState.includes("blue-overlay")) {
-      if (dataState.includes("touch-pane")) {
-        dataState += " logo-color";
-      } else {
-        dataState += " logo-white";
-      }
-    }
-  }
-
-  // Toggle visibility of set decorations
-  decorations.forEach((decoration) => {
-    var el = document.getElementById(decoration);
-    if (dataState.includes(decoration)) {
-      el.classList.add("visible");
-    } else {
-      el.classList.remove("visible");
-    };
-  });
-};
 
 function initDecorations( deck) {
   const viewport = deck.getViewportElement();
-
-  el = document.createElement('div');
-  el.setAttribute("id", "blue-overlay");
-  viewport.appendChild(el);
-
-  el = document.createElement('div');
-  el.setAttribute("id", "purple-overlay");
-  viewport.appendChild(el);
-
-  el = document.createElement('div');
-  el.setAttribute("id", "white-overlay");
-  viewport.appendChild(el);
-
-  el = document.createElement('div');
-  el.setAttribute("id", "black-overlay");
-  viewport.appendChild(el);
-
-  el = document.createElement('div');
-  el.setAttribute("id", "blue-pane-left");
-  viewport.appendChild(el);
-
-  el = document.createElement('div');
-  el.setAttribute("id", "blue-pane-right");
-  viewport.appendChild(el);
-
-  el = document.createElement('div');
-  el.setAttribute("id", "purple-half-circle-top");
-  viewport.appendChild(el);
-
-  el = document.createElement('div');
-  el.setAttribute("id", "purple-strip-bottom");
-  viewport.appendChild(el);
-
-  var el = document.createElement('div');
-  el.setAttribute("id", "yellow-strip");
-  viewport.appendChild(el);
-
-  el = document.createElement('div');
-  el.setAttribute("id", "yellow-half-strip");
-  viewport.appendChild(el);
-
-  el = document.createElement('div')
-  el.setAttribute("id", "touch-pane");
-  el.innerHTML = `<h1>Let's stay<br>in touch</h1>`
-  viewport.appendChild(el);
-
-  el = document.createElement('div');
-  el.setAttribute("id", "logo-color");
-  el.innerHTML = `<img src="./files/logo-fc.svg">`
-  viewport.appendChild(el);
-
-  el = document.createElement('div');
-  el.setAttribute("id", "logo-part-white");
-  el.innerHTML = `<img src="./files/logo-fc-part-white.svg">`
-  viewport.appendChild(el);
-
-  el = document.createElement('div');
-  el.setAttribute("id", "logo-white");
-  el.innerHTML = `<img src="./files/logo-fc-white.svg">`
-  viewport.appendChild(el);
-
-  el = document.createElement('div');
-  el.setAttribute("id", "yellow-flag");
-  el.innerHTML = `<img class="left-e" src="./files/e-logo.svg"></img>`;
-  viewport.appendChild(el);
-
-  el = document.createElement('div');
-  el.setAttribute("id", "purple-half-circle-bottom");
-  el.innerHTML = `<img class="left-e" src="./files/logo-fc-part-white.svg"></img>`
-  viewport.appendChild(el);
-
-  el = document.createElement('div');
-  el.setAttribute("id", "purple-blob");
-  el.innerHTML = `
+  const decorations = document.createElement('div');
+  decorations.setAttribute("id", "decorations");
+  decorations.innerHTML = `
+  <div id="overlay"></div>
+  <div id="blue-pane"></div>
+  <div id="purple-half-circle-top"></div>
+  <div id="purple-strip-bottom"></div>
+  <div id="yellow-strip"></div>
+  <div id="yellow-half-strip"></div>
+  <div id="touch-pane"><h1>Let's stay<br>in touch</h1></div>
+  <div id="logo-color"><img src="./files/logo-fc.svg"></div>
+  <div id="logo-part-white"><img src="./files/logo-fc-part-white.svg"></div>
+  <div id="logo-white"><img src="./files/logo-fc-white.svg"></div>
+  <div id="yellow-flag"><img class="left-e" src="./files/e-logo.svg"></img></div>
+  <div id="purple-half-circle-bottom"><img class="left-e" src="./files/logo-fc-part-white.svg"></img></div>
+  <div id="blue-strip"><div id="footer"></div></div>
+  <img id="right-e" src="./files/letter-e.svg"></img>
+  <div id="purple-blob">
     <div class="blob1"></div>
     <div class="blob2"></div>
-    <div class="box"></div>`
-  viewport.appendChild(el);
-
-  el = document.createElement('div');
-  el.setAttribute("id", "blue-strip");
-  el.innerHTML = `<div id="footer"></div>`
-  viewport.appendChild(el);
-
-  el = document.createElement('img');
-  el.setAttribute("id", "right-e-top");
-  el.setAttribute("src", "./files/letter-e.svg");
-  viewport.appendChild(el);
-
-  el = document.createElement('img');
-  el.setAttribute("id", "right-e-bottom");
-  el.setAttribute("src", "./files/letter-e.svg");
-  viewport.appendChild(el);
+    <div class="box"></div>
+  </div>
+  `;
+  viewport.appendChild(decorations);
 }
